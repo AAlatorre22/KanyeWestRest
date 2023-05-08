9c261f2a0b7fd615627ca67d472f67c3df2c3405
AAlatorre22 <126432462+AAlatorre22@users.noreply.github.com>
2023-05-08 11:34:54 -0700

    touched up

--- KanyeWestRest/Program.cs
+++ KanyeWestRest/Program.cs
@@ -8,18 +8,28 @@ namespace KanyeWestRest
         static void Main(string[] args)
         {
 
-
+            Console.WriteLine("Here is a conversation between a Ron Swanson API and a Kanye West API. Enjoy the nonsense!");
+            Console.WriteLine("Press enter to start");
+            Console.ReadLine();
             bool cont = true;
             do
             {
+                Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine($"Ron: {Quotes.RonQuote()}");
+                Console.WriteLine();
+                Console.ForegroundColor= ConsoleColor.Cyan;
                 Console.WriteLine($"Kanye: \"{Quotes.KanyeQuote()}\"");
                 Console.WriteLine();
-                Console.WriteLine("continue? Y for yes, N for no.");
+                Console.ResetColor();
+                Console.WriteLine("Continue? Y for yes, N for no.");
                 var response = Console.ReadLine().ToLower();
                 cont = (response == "y") ? true : false;
+                Console.WriteLine();
             }
             while (cont);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Thanks for visiting!");
+            Console.ResetColor();
 
 
         }
